09b2cc61ce10e0dd102b83002cd0a705c4cdb67e
Yair <ycosio@ucol.mx>
2017-03-10 20:42:33 -0600

    Displaying my location on the map

--- js/general_scripts.js
+++ js/general_scripts.js
@@ -1,3 +1,90 @@
 $( function() {
 
   } );
+
+window.onload = getMyLocation;
+
+var ourCoords = {
+ latitude: 47.624851,
+ longitude: -122.52099
+};
+
+var map;
+
+function addMarker(map, latlong, title, content) {
+	var markerOptions = {
+		position: latlong,
+		map: map,
+		title: title,
+		clickable: true
+	};
+
+	var marker = new google.maps.Marker(markerOptions);
+
+	var infoWindowOptions = {
+		content: content,
+		position: latlong
+	};
+
+	var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+	google.maps.event.addListener(marker, "click", function() {
+	infoWindow.open(map);
+	});
+}
+
+function showMap(coords) {
+	var googleLatAndLong = new google.maps.LatLng(coords.latitude,coords.longitude);
+	var mapProp= {
+	    center: googleLatAndLong,
+	    zoom: 10,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map=new google.maps.Map(document.getElementById("map"),mapProp);
+
+	var title = "Your Location";
+	var content = "You are here: " + coords.latitude + ", " + coords.longitude;
+	addMarker(map, googleLatAndLong, title, content);
+}
+
+// Get distance between points
+
+function degreesToRadians(degrees) {
+	var radians = (degrees * Math.PI)/180;
+	return radians;
+}
+
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+	var Radius = 6371; // radius of the Earth in km
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
+	Math.cos(startLatRads) * Math.cos(destLatRads) *
+	Math.cos(startLongRads - destLongRads)) * Radius;
+	return distance;
+}
+
+function displayLocation(position) {
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	var div = document.getElementById("location");
+	div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+	var km = computeDistance(position.coords, ourCoords);
+	div = document.getElementById("distance");
+	div.innerHTML = "You are " + km + " km from the WickedlySmart HQ";
+	showMap(position.coords);
+}
+
+function getMyLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(displayLocation);
+	} else {
+		alert("Oops, no geolocation support");
+	}
+}
+
+
+
+
